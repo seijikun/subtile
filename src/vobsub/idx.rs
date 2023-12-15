8aa4f83700efa09a935a7a2edb3752c7172b35d2
Gwen Lg <me@gwenlg.fr>
2023-12-15 18:08:23 +0100

    cargo: update image dependency to 0.24.7
    from 0.18

--- src/vobsub/idx.rs
+++ src/vobsub/idx.rs
@@ -35,9 +35,7 @@ named!(
 /// Parse a 3-byte hexadecimal RGB color.
 named!(
     rgb<Rgb<u8>>,
-    map!(count_fixed!(u8, call!(hex_u8), 3), |rgb| {
-        Rgb { data: rgb }
-    })
+    map!(count_fixed!(u8, call!(hex_u8), 3), |rgb| { Rgb(rgb) })
 );
 
 #[test]
@@ -45,12 +43,7 @@ fn parse_rgb() {
     use nom::IResult;
     assert_eq!(
         rgb(&b"1234ab"[..]),
-        IResult::Done(
-            &b""[..],
-            Rgb::<u8> {
-                data: [0x12, 0x34, 0xab]
-            }
-        )
+        IResult::Done(&b""[..], Rgb::<u8>([0x12, 0x34, 0xab]))
     );
 }
 
@@ -67,7 +60,7 @@ named!(
         }
         // Coerce vector to known-size slice.  Based on
         // http://stackoverflow.com/q/25428920/12089.
-        let mut result = [Rgb { data: [0, 0, 0] }; 16];
+        let mut result = [Rgb([0, 0, 0]); 16];
         <[Rgb<u8>; 16] as AsMut<_>>::as_mut(&mut result).clone_from_slice(&vec[0..16]);
         Ok(result)
     })
@@ -84,54 +77,22 @@ fn parse_palette() {
         IResult::Done(
             &[][..],
             [
-                Rgb {
-                    data: [0x00, 0x00, 0x00]
-                },
-                Rgb {
-                    data: [0xf0, 0xf0, 0xf0]
-                },
-                Rgb {
-                    data: [0xcc, 0xcc, 0xcc]
-                },
-                Rgb {
-                    data: [0x99, 0x99, 0x99]
-                },
-                Rgb {
-                    data: [0x33, 0x33, 0xfa]
-                },
-                Rgb {
-                    data: [0x11, 0x11, 0xbb]
-                },
-                Rgb {
-                    data: [0xfa, 0x33, 0x33]
-                },
-                Rgb {
-                    data: [0xbb, 0x11, 0x11]
-                },
-                Rgb {
-                    data: [0x33, 0xfa, 0x33]
-                },
-                Rgb {
-                    data: [0x11, 0xbb, 0x11]
-                },
-                Rgb {
-                    data: [0xfa, 0xfa, 0x33]
-                },
-                Rgb {
-                    data: [0xbb, 0xbb, 0x11]
-                },
-                Rgb {
-                    data: [0xfa, 0x33, 0xfa]
-                },
-                Rgb {
-                    data: [0xbb, 0x11, 0xbb]
-                },
-                Rgb {
-                    data: [0x33, 0xfa, 0xfa]
-                },
-                Rgb {
-                    data: [0x11, 0xbb, 0xbb]
-                }
+                Rgb([0x00, 0x00, 0x00]),
+                Rgb([0xf0, 0xf0, 0xf0]),
+                Rgb([0xcc, 0xcc, 0xcc]),
+                Rgb([0x99, 0x99, 0x99]),
+                Rgb([0x33, 0x33, 0xfa]),
+                Rgb([0x11, 0x11, 0xbb]),
+                Rgb([0xfa, 0x33, 0x33]),
+                Rgb([0xbb, 0x11, 0x11]),
+                Rgb([0x33, 0xfa, 0x33]),
+                Rgb([0x11, 0xbb, 0x11]),
+                Rgb([0xfa, 0xfa, 0x33]),
+                Rgb([0xbb, 0xbb, 0x11]),
+                Rgb([0xfa, 0x33, 0xfa]),
+                Rgb([0xbb, 0x11, 0xbb]),
+                Rgb([0x33, 0xfa, 0xfa]),
+                Rgb([0x11, 0xbb, 0xbb])
             ]
         )
     );
@@ -210,16 +171,6 @@ fn parse_index() {
     let idx = Index::open("./fixtures/example.idx").unwrap();
 
     //assert_eq!(idx.size(), Size { w: 1920, h: 1080 });
-    assert_eq!(
-        idx.palette()[0],
-        Rgb {
-            data: [0x00, 0x00, 0x00]
-        }
-    );
-    assert_eq!(
-        idx.palette()[15],
-        Rgb {
-            data: [0x11, 0xbb, 0xbb]
-        }
-    );
+    assert_eq!(idx.palette()[0], Rgb([0x00, 0x00, 0x00]));
+    assert_eq!(idx.palette()[15], Rgb([0x11, 0xbb, 0xbb]));
 }
