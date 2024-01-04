63febc4d1020dd7d07c65ddf965119082d832d10
Gwen Lg <me@gwenlg.fr>
2024-01-04 23:15:54 +0100

    use Parse SubError in palette instead of anyhow

--- src/vobsub/palette.rs
+++ src/vobsub/palette.rs
@@ -1,4 +1,3 @@
-use anyhow::anyhow;
 use image::Rgb;
 use nom::{
     bytes::complete::{tag, take_while_m_n},
@@ -7,6 +6,8 @@ use nom::{
     sequence::tuple,
     IResult,
 };
+
+use crate::SubError;
 
 /// Parse a single hexadecimal digit.
 fn from_hex(input: &[u8]) -> std::result::Result<u8, std::num::ParseIntError> {
@@ -36,7 +37,7 @@ pub type Palette = [Rgb<u8>; 16];
 pub fn palette(input: &[u8]) -> IResult<&[u8], Palette> {
     let res = map_res(separated_list0(tag(b", "), hex_rgb), |vec: Vec<Rgb<u8>>| {
         if vec.len() != 16 {
-            return Err(anyhow!("Palettes must have 16 entries"));
+            return Err(SubError::Parse("Palettes must have 16 entries".into()));
         }
         // Coerce vector to known-size slice.  Based on
         // http://stackoverflow.com/q/25428920/12089.
