2515364bd7c65cfc60d2a1631f9a6f28782ec2ef
Gwen Lg <me@gwenlg.fr>
2024-05-30 16:31:34 +0200

    feat(image): add trait ToOcrImage and struct ToOcrImageOpt.
    - ToOcrImage define the fonctionnality to create an image for OCR
      with options
    - ToOcrImageOpt is a struct to store options for image creation.
      Options border and colors (text and background)

--- src/image/mod.rs
+++ src/image/mod.rs
@@ -4,6 +4,7 @@ mod utils;
 pub use utils::{dump_images, DumpError};
 
 use crate::content::Area;
+use image::{GrayImage, Luma};
 
 /// Define access to Size of an Image. Used for Subtitle content.
 pub trait ImageSize {
@@ -30,4 +31,33 @@ where
     fn height(&self) -> u32 {
         u32::from(self.area().height())
     }
+}
+
+/// Options for image generation.
+#[derive(Debug, Clone, Copy)]
+pub struct ToOcrImageOpt {
+    /// Number of border pixels to add on the input image
+    pub border: u32,
+    /// Color of the text
+    pub text_color: Luma<u8>,
+    /// Color of the background
+    pub background_color: Luma<u8>,
+}
+
+// Implement [`Default`] for [`ToOcrImageOpt`] with a border of 5 pixel
+// and colors black for text and white for background.
+impl Default for ToOcrImageOpt {
+    fn default() -> Self {
+        Self {
+            border: 5,
+            text_color: Luma([0]),
+            background_color: Luma([255]),
+        }
+    }
+}
+
+/// Generate a `GrayImage` adapted for `OCR` from self.
+pub trait ToOcrImage {
+    /// Generate the image for `OCR` in `GrayImage` format.
+    fn image(&self, opt: &ToOcrImageOpt) -> GrayImage;
 }
