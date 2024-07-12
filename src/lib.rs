7a8c86b5da576fc90f925f58e74726df2b3480fc
Gwen Lg <me@gwenlg.fr>
2024-07-12 16:10:07 +0200

    move lints configuration from lib.rs to Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -17,28 +17,6 @@
 //! Your feedback and contributions are welcome!  Please see
 //! [Subtile](https://github.com/gwen-lg/subtile) on GitHub for details.
 
-#![deny(missing_docs)]
-#![deny(unused_imports)]
-#![deny(clippy::bind_instead_of_map)]
-#![deny(clippy::borrowed_box)]
-#![deny(clippy::cast_lossless)]
-#![deny(clippy::clone_on_copy)]
-#![deny(clippy::derive_partial_eq_without_eq)]
-#![deny(clippy::doc_markdown)]
-#![deny(clippy::extra_unused_lifetimes)]
-#![deny(clippy::if_not_else)]
-#![deny(clippy::match_same_arms)]
-#![deny(clippy::missing_const_for_fn)]
-#![deny(clippy::missing_errors_doc)]
-#![deny(clippy::missing_fields_in_debug)]
-#![deny(clippy::missing_panics_doc)]
-#![deny(clippy::must_use_candidate)]
-#![deny(clippy::or_fun_call)]
-#![deny(clippy::trivially_copy_pass_by_ref)]
-#![deny(clippy::uninlined_format_args)]
-#![deny(clippy::use_self)]
-#![deny(clippy::unreadable_literal)]
-#![deny(clippy::useless_conversion)]
 // For error-chain.
 #![recursion_limit = "1024"]
 
