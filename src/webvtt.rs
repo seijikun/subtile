26976de9fa39e4f133fa7894e031402f64a9e71b
Gwen Lg <me@gwenlg.fr>
2025-02-13 17:53:51 +0100

    feat(webvtt): add write_line public function

--- src/webvtt.rs
+++ src/webvtt.rs
@@ -1,7 +1,7 @@
 //! `WebVTT` functionality
-use std::fmt;
+use std::{fmt, io};
 
-use crate::time::TimePoint;
+use crate::time::{TimePoint, TimeSpan};
 
 /// Extend `TimePoint` for implement `WebVTT` specific `Display`.
 #[repr(transparent)]
@@ -17,4 +17,18 @@ impl fmt::Display for TimePointVtt {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         self.0.fmt_separator(f, '.')
     }
+}
+
+/// Write a subtitles line in `vtt` format
+/// # Errors
+///
+/// Will return `Err` if writing in `writer` return an `Err`.
+pub fn write_line(
+    writer: &mut impl io::Write,
+    time: &TimeSpan,
+    text: &str,
+) -> Result<(), io::Error> {
+    let start = TimePointVtt(time.start);
+    let end = TimePointVtt(time.end);
+    writeln!(writer, "{start} --> {end}\n{text}\n")
 }
